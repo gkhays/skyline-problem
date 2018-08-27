3b49e65f1f71be5ff2793a641495cd912830f758
gkhays <gkhays@gmail.com>
2018-08-27 17:53:23 -0500

    Add Input Controls
    It has been faster to just use demo data to get this working. But the idea is to let the viewer enter his or her own data.

--- src/org/poc/algorithm/skyline/SolutionFrame.java
+++ src/org/poc/algorithm/skyline/SolutionFrame.java
@@ -1,8 +1,15 @@
 package org.poc.algorithm.skyline;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
 import java.util.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,13 +25,19 @@ public class SolutionFrame extends JFrame {
 	
 	// TODO: Adjust the size of the frame depending on the width and number of
 	// buildings.
-	public static final int WIDTH = 900;
+	public static final int WIDTH = 625;
 	public static final int HEIGHT = 300;
+	
+	JButton btnAdd;
+	JTextField left;
+	JTextField right;
+	JTextField height;
 	
 	JPanel contentPane;
 	SolutionPane solutionPane;
 	
 	public SolutionFrame(List<SkylineProblem.Building> buildingList) {
+		// TODO: Shouldn't we do all this initialization in the solution panel?
 		contentPane = new JPanel();
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
@@ -33,8 +46,9 @@ public class SolutionFrame extends JFrame {
 		contentPane.add(solutionPane, BorderLayout.CENTER);
 
 		JPanel inputPanel = new JPanel();
+		inputPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		
-		JTextField left = new JTextField();
+		left = new JTextField();
 		left.setColumns(3);
 		inputPanel.add(left);
 		JLabel leftLabel = new JLabel("Left:");
@@ -42,7 +56,7 @@ public class SolutionFrame extends JFrame {
 		leftLabel.setLabelFor(left);
 		inputPanel.add(leftLabel);
 		
-		JTextField right = new JTextField();
+		right = new JTextField();
 		right.setColumns(3);
 		inputPanel.add(right);
 		JLabel rightLabel = new JLabel("Right:");
@@ -50,7 +64,7 @@ public class SolutionFrame extends JFrame {
 		leftLabel.setLabelFor(right);
 		inputPanel.add(rightLabel);
 		
-		JTextField height = new JTextField();
+		height = new JTextField();
 		height.setColumns(3);
 		inputPanel.add(height);
 		JLabel heightLabel = new JLabel("Height:");
@@ -58,7 +72,35 @@ public class SolutionFrame extends JFrame {
 		leftLabel.setLabelFor(height);
 		inputPanel.add(heightLabel);
 		
-		contentPane.add(inputPanel, BorderLayout.WEST);
+		btnAdd = new JButton("Add Building");
+		btnAdd.setToolTipText("Add a new building");
+		inputPanel.add(btnAdd);
+		
+		long when = 0;
+		JCheckBox demoCheckBox = new JCheckBox("Use Demo Data");
+		demoCheckBox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				disableTextInput();
+			}			
+		});
+		demoCheckBox.setSelected(true);
+		// Fire checkbox event.
+		new ActionEvent(demoCheckBox, ActionEvent.ACTION_PERFORMED, "Demo", when, 0);
+		disableTextInput();
+		inputPanel.add(demoCheckBox);
+		
+		contentPane.add(inputPanel, BorderLayout.NORTH);
+	}
+	
+	void disableTextInput() {
+		left.setEnabled(false);
+		left.setBackground(Color.lightGray);
+		right.setEnabled(false);
+		right.setBackground(Color.lightGray);
+		height.setEnabled(false);
+		height.setBackground(Color.lightGray);
+		btnAdd.setEnabled(false);
 	}
 	
 }
